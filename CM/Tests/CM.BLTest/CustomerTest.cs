3ed75ec2a0e7e671ea824f948845b0d51e9af90a
Blair Brownlie <bbrownli@asu.edu>
2021-02-25 12:15:23 -0700

    adding one test

---
+++ CM/Tests/CM.BLTest/CustomerTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using CM.BL;
+
+namespace CM.BLTest
+{
+    [TestClass]
+    public class CustomerTest
+    {
+        [TestMethod]
+        public void FullNameTestValid()
+        {
+            // -- Arrange
+            Customer customer = new Customer();
+            customer.FirstName = "Bilbo";
+            customer.LastName = "Baggins";
+            // -- Act
+            string actual = "Baggins, Bilbo";
+            // -- Assert
+            Assert.AreEqual(customer.FullName, actual);
+        }
+    }
+}
